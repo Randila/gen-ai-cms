9b2ed6b76270e5431be868a9389a8edbda21934c
randila <89266834+Randila@users.noreply.github.com>
2025-12-25 23:32:08 +0530

    added new document Type = Blogs and defined a schema.

---
+++ schemaTypes/blogType.ts
@@ -0,0 +1,70 @@
+import { defineField, defineType } from "sanity";
+
+export const blogType = defineType({
+    name: 'Blog',
+    title : 'Blog',
+    type : 'document',
+    groups :[
+        {name : 'blog_card' , title : 'Blog Card', default:true},
+        {name : 'blog_body' , title : 'Blog Body'},
+    ],
+    fields : [
+        defineField({
+            name : 'title',
+            type : 'string',
+            description : 'Title of the Blog.',
+            validation:(rule) => rule.required(),
+            group: 'blog_card',
+        }),
+        defineField({
+            name: 'slug',
+            type: 'slug',
+            options:{source : 'title'},
+            validation:(rule)=>rule.required().error(`Required to generate a page for the blog`),
+            group : 'blog_card'
+        }),
+        defineField({
+            name : 'subheading',
+            type : 'string',
+            description : 'subheading for the blog tile',
+            // validation:(rule) => rule.required(),
+            group: 'blog_card',
+        }),
+        defineField({
+            name : 'Blog_Thumbnail',
+            type : 'image',
+            description : 'image for blog tile',
+            validation:(rule) => rule.required(),
+            group: 'blog_card',
+        }),
+        defineField({
+            name : 'Date',
+            type : 'date',
+            description : 'date of publication',
+            // validation:(rule) => rule.required(),
+            group: 'blog_card',
+        }),
+        defineField({
+            name : 'Read_Time',
+            type : 'number',
+            initialValue: 1,
+            description : 'Estimated time to read the whole blog (Enter in minutes)',
+            // validation:(rule) => rule.required(),
+            group: 'blog_card',
+        }),
+        defineField({
+            name : 'Blog_Image',
+            type : 'image',
+            description : 'Main background image for the blog.',
+            // validation:(rule) => rule.required(),
+            group: 'blog_body',
+        }),
+        defineField({
+            name : 'Blog_Content',
+            type : 'array',
+            of : [{type : 'block'}],
+            description : 'write your blog post',
+            group: 'blog_body',
+        })
+    ]
+});
--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,3 +1,4 @@
+import { blogType } from "./blogType";
 import { programType } from "./programType";
 
-export const schemaTypes = [programType]
+export const schemaTypes = [programType , blogType]
